50da5754103732992b914b109dd15ee6c7129b1d
Aditya Saxena <aditya.saxena33@gmail.com>
2021-02-21 20:24:27 +0530

    Initialised

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLcO2BJQoQ9RUDSdA4W_faLWoiPzJf-dM",
+  authDomain: "gitapp-60276.firebaseapp.com",
+  databaseURL: "https://gitapp-60276-default-rtdb.firebaseio.com",
+  projectId: "gitapp-60276",
+  storageBucket: "gitapp-60276.appspot.com",
+  messagingSenderId: "641912218778",
+  appId: "1:641912218778:web:8e26139df7b369d900ab23",
+  measurementId: "G-DX7X1G06EX",
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
